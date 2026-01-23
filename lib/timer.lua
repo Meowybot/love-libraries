b6db4747e4e5a37d712a1ffc9536e713b1238038
Meowybot <jorgedidier149@gmail.com>
2026-01-22 19:09:51 -0600

    Update timer.lua

--- lib/timer.lua
+++ lib/timer.lua
@@ -1,20 +1,21 @@
 local timer = {}
 local metatimer = {}
-metatimer.__index = metatimer
+metatimer.basic = {}
+metatimer.basic.__index = metatimer
 
-function metatimer:pause()
+function metatimer.basic:pause()
   if not self.paused then
     self.paused = true
   end
 end
 
-function metatimer:resume()
+function metatimer.basic:resume()
   if self.paused then
     self.paused = false
   end
 end
 
-function metatimer:update(dt)
+function metatimer.basic:update(dt)
   if not self.paused then
     self.iti = self.iti + dt
     if self.iti >= self.timeset then
@@ -24,6 +25,16 @@ function metatimer:update(dt)
   end
 end
 
-function metatimer:finish()
+function metatimer.basic:finish()
   self.iti = self.timeset
+end
+
+function timer.newBasic(time, onEnd, startpaused)
+  local newti = {}
+  newti.timeset = time or 2
+  newti.onEnd = onEnd or function() end
+  newti.paused = startpaused or false
+  newti.iti = 0
+  setmetatable(newti, metatimer.basic)
+  return newti
 end
