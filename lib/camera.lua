d8764bdef6a4b1c1c1305278a6fa109aab656a0d
Meowybot <jorgedidier149@gmail.com>
2026-01-24 18:44:27 -0600

    Update camera.lua

--- lib/camera.lua
+++ lib/camera.lua
@@ -1,4 +1,34 @@
 local cam = {}
 local cammeta = {}
 
+function cammeta:lookAt(x, y)
+  self.x = x
+  self.y = y
+end
+
+function cammeta:changeZoom(zoom)
+  self.zoom = zoom
+end
+
+function cammeta:changeRot(rot)
+  self.rot = rot
+end
+
+function cammeta:move(x, y)
+  self.x = self.x + x
+  self.y = self.y + y
+end
+
+function cammeta:zoom(zoom)
+  self.zoom = self.zoom + zoom
+end
+
+function cammeta:rotate(rot)
+  self.rot = self.rot + rot
+end
+
+function cam.new(x, y, zoom, rot)
+  --replace soon
+end
+
 return cam
